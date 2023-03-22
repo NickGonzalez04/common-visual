f2fedcb88395f0eec8a01df178d55e52cf8a6eb6
nicholas <gonzaleznick5@gmail.com>
2023-03-22 15:35:07 -0400

    redploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,10 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-semi': 'off'
   }
 }
